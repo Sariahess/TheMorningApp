c46ba683e122c986211111c4bcd67d97075b9f41
saria <sariahess@gmail.com>
2020-11-16 05:28:55 -0600

    Add project files.

---
+++ MorningApp.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace TheMorningApp
+{
+    partial class MorningApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcomeMessage = new System.Windows.Forms.Label();
+            this.City = new System.Windows.Forms.Label();
+            this.Country = new System.Windows.Forms.Label();
+            this.Province = new System.Windows.Forms.Label();
+            this.IP_Address = new System.Windows.Forms.Label();
+            this.Temperature = new System.Windows.Forms.Label();
+            this.currency = new System.Windows.Forms.Label();
+            this.getIP = new System.Windows.Forms.Label();
+            this.getCity = new System.Windows.Forms.Label();
+            this.getProvince = new System.Windows.Forms.Label();
+            this.getCountry = new System.Windows.Forms.Label();
+            this.getTemp = new System.Windows.Forms.Label();
+            this.getCAD = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // welcomeMessage
+            // 
+            this.welcomeMessage.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.welcomeMessage.AutoSize = true;
+            this.welcomeMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeMessage.ForeColor = System.Drawing.Color.Purple;
+            this.welcomeMessage.Location = new System.Drawing.Point(278, 26);
+            this.welcomeMessage.Name = "welcomeMessage";
+            this.welcomeMessage.Size = new System.Drawing.Size(359, 63);
+            this.welcomeMessage.TabIndex = 0;
+            this.welcomeMessage.Text = "Hello Master!";
+            // 
+            // City
+            // 
+            this.City.AutoSize = true;
+            this.City.BackColor = System.Drawing.SystemColors.Highlight;
+            this.City.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.City.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.City.Location = new System.Drawing.Point(42, 287);
+            this.City.Name = "City";
+            this.City.Size = new System.Drawing.Size(83, 41);
+            this.City.TabIndex = 1;
+            this.City.Text = "City";
+            // 
+            // Country
+            // 
+            this.Country.AutoSize = true;
+            this.Country.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Country.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Country.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Country.Location = new System.Drawing.Point(799, 287);
+            this.Country.Name = "Country";
+            this.Country.Size = new System.Drawing.Size(145, 41);
+            this.Country.TabIndex = 2;
+            this.Country.Text = "Country";
+            // 
+            // Province
+            // 
+            this.Province.AutoSize = true;
+            this.Province.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Province.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Province.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Province.Location = new System.Drawing.Point(433, 287);
+            this.Province.Name = "Province";
+            this.Province.Size = new System.Drawing.Size(150, 41);
+            this.Province.TabIndex = 3;
+            this.Province.Text = "Province";
+            // 
+            // IP_Address
+            // 
+            this.IP_Address.AutoSize = true;
+            this.IP_Address.BackColor = System.Drawing.SystemColors.Highlight;
+            this.IP_Address.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IP_Address.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.IP_Address.Location = new System.Drawing.Point(42, 156);
+            this.IP_Address.Name = "IP_Address";
+            this.IP_Address.Size = new System.Drawing.Size(186, 41);
+            this.IP_Address.TabIndex = 4;
+            this.IP_Address.Text = "IP Address";
+            // 
+            // Temperature
+            // 
+            this.Temperature.AutoSize = true;
+            this.Temperature.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Temperature.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Temperature.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.Temperature.Location = new System.Drawing.Point(44, 495);
+            this.Temperature.Name = "Temperature";
+            this.Temperature.Size = new System.Drawing.Size(222, 41);
+            this.Temperature.TabIndex = 5;
+            this.Temperature.Text = "Temperature";
+            // 
+            // currency
+            // 
+            this.currency.AutoSize = true;
+            this.currency.BackColor = System.Drawing.SystemColors.Highlight;
+            this.currency.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currency.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.currency.Location = new System.Drawing.Point(41, 640);
+            this.currency.Name = "currency";
+            this.currency.Size = new System.Drawing.Size(313, 41);
+            this.currency.TabIndex = 7;
+            this.currency.Text = "Currency Exchange";
+            // 
+            // getIP
+            // 
+            this.getIP.AutoSize = true;
+            this.getIP.BackColor = System.Drawing.Color.White;
+            this.getIP.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getIP.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.getIP.Location = new System.Drawing.Point(431, 148);
+            this.getIP.Name = "getIP";
+            this.getIP.Size = new System.Drawing.Size(312, 52);
+            this.getIP.TabIndex = 12;
+            this.getIP.Text = "my IP Address";
+            // 
+            // getCity
+            // 
+            this.getCity.AutoSize = true;
+            this.getCity.BackColor = System.Drawing.Color.White;
+            this.getCity.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getCity.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.getCity.Location = new System.Drawing.Point(41, 345);
+            this.getCity.Name = "getCity";
+            this.getCity.Size = new System.Drawing.Size(174, 52);
+            this.getCity.TabIndex = 13;
+            this.getCity.Text = "my city";
+            // 
+            // getProvince
+            // 
+            this.getProvince.AutoSize = true;
+            this.getProvince.BackColor = System.Drawing.Color.White;
+            this.getProvince.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getProvince.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.getProvince.Location = new System.Drawing.Point(431, 345);
+            this.getProvince.Name = "getProvince";
+            this.getProvince.Size = new System.Drawing.Size(259, 52);
+            this.getProvince.TabIndex = 14;
+            this.getProvince.Text = "my province";
+            // 
+            // getCountry
+            // 
+            this.getCountry.AutoSize = true;
+            this.getCountry.BackColor = System.Drawing.Color.White;
+            this.getCountry.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getCountry.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.getCountry.Location = new System.Drawing.Point(797, 345);
+            this.getCountry.Name = "getCountry";
+            this.getCountry.Size = new System.Drawing.Size(249, 52);
+            this.getCountry.TabIndex = 15;
+            this.getCountry.Text = "my country";
+            // 
+            // getTemp
+            // 
+            this.getTemp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.getTemp.AutoSize = true;
+            this.getTemp.BackColor = System.Drawing.Color.White;
+            this.getTemp.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getTemp.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.getTemp.Location = new System.Drawing.Point(433, 484);
+            this.getTemp.Name = "getTemp";
+            this.getTemp.Size = new System.Drawing.Size(125, 52);
+            this.getTemp.TabIndex = 16;
+            this.getTemp.Text = "temp";
+            // 
+            // getCAD
+            // 
+            this.getCAD.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.getCAD.AutoSize = true;
+            this.getCAD.BackColor = System.Drawing.Color.White;
+            this.getCAD.Font = new System.Drawing.Font("MV Boli", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getCAD.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.getCAD.Location = new System.Drawing.Point(433, 632);
+            this.getCAD.Name = "getCAD";
+            this.getCAD.Size = new System.Drawing.Size(106, 52);
+            this.getCAD.TabIndex = 17;
+            this.getCAD.Text = "CAD";
+            // 
+            // MorningApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1424, 740);
+            this.Controls.Add(this.getCAD);
+            this.Controls.Add(this.getTemp);
+            this.Controls.Add(this.getCountry);
+            this.Controls.Add(this.getProvince);
+            this.Controls.Add(this.getCity);
+            this.Controls.Add(this.getIP);
+            this.Controls.Add(this.currency);
+            this.Controls.Add(this.Temperature);
+            this.Controls.Add(this.IP_Address);
+            this.Controls.Add(this.Province);
+            this.Controls.Add(this.Country);
+            this.Controls.Add(this.City);
+            this.Controls.Add(this.welcomeMessage);
+            this.Name = "MorningApp";
+            this.Text = "Morning Application";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label welcomeMessage;
+        private System.Windows.Forms.Label City;
+        private System.Windows.Forms.Label Country;
+        private System.Windows.Forms.Label Province;
+        private System.Windows.Forms.Label IP_Address;
+        private System.Windows.Forms.Label Temperature;
+        private System.Windows.Forms.Label currency;
+        private System.Windows.Forms.Label getIP;
+        private System.Windows.Forms.Label getCity;
+        private System.Windows.Forms.Label getProvince;
+        private System.Windows.Forms.Label getCountry;
+        private System.Windows.Forms.Label getTemp;
+        private System.Windows.Forms.Label getCAD;
+    }
+}
+
---
+++ MorningApp.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace TheMorningApp
+{
+    public partial class MorningApp : Form
+    {
+        public MorningApp()
+        {
+            InitializeComponent();
+
+            // start up at Windows start
+            StartUpConfig();
+
+            // show user name
+            welcomeMessage.Text = $"Good morning, {Environment.UserName.ToUpper()}!!";
+
+            // ipDATA : location
+            string ipdataAPI = "9ba5af5e2d8235046f47517c8207fbe015965c2d2bb49b42af325658";
+            string addressForLocation = "https://api.ipdata.co?api-key=";
+
+            IpData.Rootobject locationObj = JsonConvert.DeserializeObject<IpData.Rootobject>(ReturnString(ipdataAPI, addressForLocation));
+
+            getIP.Text = locationObj.ip;
+            getCity.Text = locationObj.city;
+            getProvince.Text = locationObj.region;
+            getCountry.Text = locationObj.country_name;
+
+
+            // weather : temperature
+            string weatherAPI = "92e5f185dca1f202a53dedfb827f6dc9";
+            string addressForTemperature = "http://api.openweathermap.org/data/2.5/weather?q=" + $"{locationObj.city}&appid=";
+
+            WeatherData.Rootobject weatherObj = JsonConvert.DeserializeObject<WeatherData.Rootobject>(ReturnString(weatherAPI, addressForTemperature));
+
+            getTemp.Text = (weatherObj.main.temp - 273.15).ToString("0.##") + "  °C";
+
+
+            // currency : Korean Won vs. Canadian Dollar
+            string currencyAPI = "";
+            string addressForCurrency = "https://api.exchangeratesapi.io/latest";
+
+            CurrencyData.Rootobject rateObj = JsonConvert.DeserializeObject<CurrencyData.Rootobject>(ReturnString(currencyAPI, addressForCurrency));
+
+            getCAD.Text = $"{(rateObj.rates.KRW / rateObj.rates.CAD).ToString("0.##")}" + " KRW / CAD";
+        }
+
+        public string ReturnString(string apiKey, string apiAddress)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage response = httpClient.GetAsync($"{apiAddress}" + $"{apiKey}").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResult = response.Content.ReadAsStringAsync().Result;
+                return jsonResult;
+            }
+            else
+            {
+                return response.StatusCode + response.ReasonPhrase;
+            }
+        }
+
+        public void StartUpConfig()
+        {
+            RegistryKey key = Microsoft.Win32.Registry.CurrentUser
+                .OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            key.SetValue("TheMorningApp", Application.ExecutablePath);
+        }
+    }
+}
